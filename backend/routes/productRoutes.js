a5d90aa2c458531f90eeac61f5a7eff3cb6215ab
GaneshCS316 <ganeshkrishnaprasad2019@gmail.com>
2025-02-10 15:18:45 +0530

    project completed

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { 
+    createProduct,
+     getProducts,
+     getProduct,
+     updateProduct,
+     deleteProduct
+    
+    } from "../controllers/productController.js";
+const router = express.Router();
+
+// Define your routes here
+router.get("/", getProducts);
+router.get("/:id", getProduct);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+
+export default router;
